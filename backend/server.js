f10597a04666186a945917be96f6f9aba794c792
Njosephine <josephinenanyonga389@gmail.com>
2024-10-21 17:21:19 +0300

    setting up MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from "./config/mongodb.js"
-import connectCloudinary from "./config/cloudinary.js"
-import userRouter from "./routes/userRoute.js"
-import doctorRouter from "./routes/doctorRoute.js"
-import adminRouter from "./routes/adminRoute.js"
+import connectDB from "./config/Mongodb.js"
+import connectCloudinary from "./config/Cloudinary.js"
+// import userRouter from "./routes/userRoute.js"
+// import doctorRouter from "./routes/doctorRoute.js"
+// import adminRouter from "./routes/adminRoute.js"
 
 // app config
 const app = express()
@@ -17,10 +17,10 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
-// api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/admin", adminRouter)
-app.use("/api/doctor", doctorRouter)
+// // api endpoints
+// app.use("/api/user", userRouter)
+// app.use("/api/admin", adminRouter)
+// app.use("/api/doctor", doctorRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working")

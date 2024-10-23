d51f89efd71605b8a5e14d804b9ea0861386beba
Njosephine <josephinenanyonga389@gmail.com>
2024-10-23 08:37:10 +0300

    Models

---
+++ backend/models/UserModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+     name: {type: String, required: true},
+     email: {type:   String,  required: true, unqiue: true},
+     phone: {type: String, default: '000000000'},
+     image: {type: String, default: ''},        
+     
+
+     address: {type: Object, default: { line1: '' ,line2: ''}},
+     gender: {type: String, default: 'Not Selected'},
+     dob: {type: String, default: 'Not Selected'},
+     password: {type: String, required: true},
+})
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema)
+export default userModel;

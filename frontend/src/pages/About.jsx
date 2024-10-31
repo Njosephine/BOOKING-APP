04fc16092a2471fd3055dab2894f9982fa506a00
Njosephine <josephinenanyonga389@gmail.com>
2024-10-31 10:51:29 +0300

    frontend

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-center text-2xl pt-10 text-[#707070]'>
+        <p>ABOUT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to MedicHospital, your reliable partner in healthcare. We simplify appointment booking and health record management for patients.</p>
+          <p>With a focus on innovative healthcare solutions, MedicHospital is dedicated to enhancing your experience and providing top-notch support at every stage of care.</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our vision is to streamline healthcare, bridging the gap between patients and providers for accessible, timely care.</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>EFFICIENCY:</b>
+          <p>Easy scheduling to fit your lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>CONVENIENCE:</b>
+          <p>Access trusted healthcare providers near you.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>PERSONALIZATION:</b>
+          <p>Custom reminders and health tips to keep you on track.</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default About

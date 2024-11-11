20d2b036f8eb9e96556ba9a90e3a0717ee53399b
Njosephine <josephinenanyonga389@gmail.com>
2024-11-11 16:49:42 +0300

    console

--- backend/middleware/authUser.js
+++ backend/middleware/authUser.js
@@ -7,6 +7,10 @@ const authUser = async (req, res, next) => {
         return res.json({ success: false, message: 'Not Authorized Login Again' })
     }
     try {
+
+        
+            console.log("Received Token:", token);
+            console.log("JWT Secret:", process.env.JWT_SECRET);
         const token_decode = jwt.verify(token, process.env.JWT_SECRET)
         req.body.userId = token_decode.id
         next()
